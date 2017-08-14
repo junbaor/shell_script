353e550a5f74b2e3442f331e8822255b3c0f6504
junbaor <junbaor@qq.com>
2017-08-14 17:25:01 +0800

    upgrade kernel to v4.12.6

--- kernel_bbr.sh
+++ kernel_bbr.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-mkdir kernel && cd kernel
+mkdir temp-kernel && cd temp-kernel
 
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10/linux-headers-4.10.0-041000_4.10.0-041000.201702191831_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10/linux-headers-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10/linux-image-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.12.6/linux-headers-4.12.6-041206_4.12.6-041206.201708111232_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.12.6/linux-headers-4.12.6-041206-generic_4.12.6-041206.201708111232_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.12.6/linux-image-4.12.6-041206-generic_4.12.6-041206.201708111232_amd64.deb
 
 dpkg -i linux-image-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
 dpkg -i linux-headers-4.10.0-041000_4.10.0-041000.201702191831_all.deb
 dpkg -i linux-headers-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
 
+rm -rf temp-kernel
+
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+
 echo '10s 后重启生效'
 sleep 10
+
 reboot
